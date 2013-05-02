9de01a32cdf800cb9fc39fc125ad9e73e45df913
Methus Detchusananart <m.detch@gmail.com>
2013-05-02 17:52:33 +0700

    done

---
+++ client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleApplication1
+{
+    class Client
+    {
+        public static void Main(String[] args)
+        {
+            TcpClient socketForServer = new TcpClient("127.0.0.1", 10099);
+            NetworkStream stream = socketForServer.GetStream();
+            System.IO.StreamReader reader = new System.IO.StreamReader(stream);
+            System.IO.StreamWriter writer = new System.IO.StreamWriter(stream);
+            Console.WriteLine("This is client.");
+            Console.Write("type:");
+            String str = Console.ReadLine();
+            while (str != "exit")
+            {
+                writer.WriteLine(str);
+                writer.Flush();
+                Console.Write("type:");
+                str = Console.ReadLine();
+            }
+            if (str == "exit")
+            {
+                writer.WriteLine(str);
+                writer.Flush();
+            }
+            stream.Close();
+        }
+    }
+}
---
+++ server.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+
+    class Server
+    {
+
+        public static void Main(string[] args)
+        {
+            TcpListener tcp = new TcpListener(10099);
+            tcp.Start();
+            Console.WriteLine("This is server.");
+            Socket client = tcp.AcceptSocket();
+            if (client.Connected)
+            {
+                NetworkStream stream = new NetworkStream(client);
+                System.IO.StreamWriter writer = new System.IO.StreamWriter(stream);
+                System.IO.StreamReader reader = new System.IO.StreamReader(stream);
+
+                while (true) 
+                {
+                    String input = reader.ReadLine();
+                    Console.WriteLine("Msg from client : " + input);
+                    if (input == "exit")
+                    {
+                        break;
+                    }
+                }
+                stream.Close();
+                reader.Close();
+                writer.Close();
+            }
+            client.Close();
+        }
+    }
+}
